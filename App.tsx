daada25abaeb220af05996e79caea2be36a67f1c
Guilherme Freitas <hfdsguilherme@gmail.com>
2021-09-06 20:09:05 -0300

    chore: fake api

--- App.tsx
+++ App.tsx
@@ -1,4 +1,9 @@
+
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+import { AppProvider } from './src/hooks';
+
 import {
   useFonts,
   Inter_400Regular,
@@ -9,9 +14,9 @@ import {
   Archivo_500Medium,
   Archivo_600SemiBold
 } from '@expo-google-fonts/archivo';
-import { Home } from './src/screens/Home';
-import AppLoading from 'expo-app-loading';
-import { ThemeProvider } from 'styled-components'
+
+import { Routes } from './src/routes';
+
 import theme from './src/styles/theme';
 
 export default function App() {
@@ -21,15 +26,17 @@ export default function App() {
     Archivo_400Regular,
     Archivo_500Medium,
     Archivo_600SemiBold
-  })
+  });
 
-  if(!fontsLoaded) {
+  if (!fontsLoaded) {
     return <AppLoading />
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <AppProvider>
+        <Routes />
+      </AppProvider>
     </ThemeProvider>
   )
 }
